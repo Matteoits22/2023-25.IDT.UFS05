756ca872e785b6519133c0cecb537d74af3f0600
Matteoits22 <matteo.laini@itsrizzoli.it>
2024-07-01 19:24:28 +0000

    aa

--- 20240618/main.tf
+++ 20240618/main.tf
@@ -97,7 +97,7 @@ resource "azurerm_app_service_source_control" "python_scm" {
 resource "azurerm_mysql_flexible_server" "example" {
   name                   = "its-rizzoli-idt-mysql-${random_integer.ri.result}"
   resource_group_name    = azurerm_resource_group.rg.name
-  location               = azurerm_resource_group.rg.location
+  location               = var.AZURE_REGION
   administrator_login    = "psqladmin"
   // WARN DONT DO THIS, USE SecOps services like Doppler and Azure Key Vault
   administrator_password = "H@Sh1CoR3!"
